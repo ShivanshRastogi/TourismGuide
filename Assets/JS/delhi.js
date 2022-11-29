a15f34f0d81607477b87cda2682e154fbcf44a7c
ShivanshRastogi <72811522+ShivanshRastogi@users.noreply.github.com>
2022-11-29 21:06:06 +0530

    Update delhi.js

--- Assets/JS/delhi.js
+++ Assets/JS/delhi.js
@@ -3,6 +3,7 @@ const instanceUrl = window.location.origin;
 $(()=>{
     populateIndiaGate();
     populateLodhiGarden();
+    populateCalanguteBeach();
 })
 
 function populateIndiaGate(){
@@ -52,6 +53,33 @@ function populateLodhiGarden(){
                       }
                   });
                   $('#lodhidiv').html(htmlStr);
+              }
+            });
+          }
+        })
+        .catch((err) => {});
+}
+
+function populateCalanguteBeach(){
+    var htmlStr = '<p align="left"><b><h3>Reviews</h3></b></p>';
+    fetch(instanceUrl + "/places", {
+        method: "GET", // POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.status == 200) {
+            response.json().then((data) => {
+              if(data != null && data != undefined && data.length > 0){
+                  data.forEach((result) =>{
+                      if(result.place === 'Calangute Beach'){
+                          htmlStr += '<div class=\"sub-container2\">';
+                          htmlStr += '<p class="place-text"><span>'+result.name+'</span></p>';
+                          htmlStr += '<p class="place-text">'+result.review+'</p></div>';
+                      }
+                  });
+                  $('#calangutebeachdiv').html(htmlStr);
               }
             });
           }
